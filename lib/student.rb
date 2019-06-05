68b8bd8ee0d654ad7156787ddaba6c566cfd373b
James Declerus <james.declerus@esih.edu>
2019-06-05 12:30:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,12 +21,9 @@ end
        SELECT *
        FROM students
      SQL
-
      DB[:conn].execute(sql).map do |row|
        self.new_from_db(row)
      end
-
-
   end
 
   def self.find_by_name(name)
