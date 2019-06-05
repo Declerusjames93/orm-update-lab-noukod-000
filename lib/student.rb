831201a516a0cf616e6abb6db9266181dd092981
James Declerus <james.declerus@esih.edu>
2019-06-05 12:23:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,9 @@ require_relative "../config/environment.rb"
 class Student
 attr_accessor :name, :id , :grade
 def initialize(name,grade, id= nil)
+  @name = name 
+  @grade  = grade 
+  @id  = id
 
 end
 
